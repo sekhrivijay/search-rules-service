e10f460eb8879500c03bad499da061d72dfcd2eb
Sekhri <vsekhri@FTDCORP.NET>
2017-11-06 17:34:53 -0600

    Rule for redirect

--- src/main/java/com/services/micro/rules/search/bl/repository/RuleRepository.java
+++ src/main/java/com/services/micro/rules/search/bl/repository/RuleRepository.java
@@ -4,6 +4,6 @@ import com.services.micro.rules.search.api.request.RuleServiceRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RuleRepository extends MongoRepository<RuleServiceRequest, String> {
-    public RuleServiceRequest findByPackageNameAndRuleName(String packageName, String ruleName);
-    public RuleServiceRequest findByPackageNameAndRuleNameAndServiceNameAndEnvironment(String packageName, String ruleName, String serviceName, String environment);
+    RuleServiceRequest findByPackageNameAndRuleName(String packageName, String ruleName);
+    RuleServiceRequest findByPackageNameAndRuleNameAndServiceNameAndEnvironment(String packageName, String ruleName, String serviceName, String environment);
 }

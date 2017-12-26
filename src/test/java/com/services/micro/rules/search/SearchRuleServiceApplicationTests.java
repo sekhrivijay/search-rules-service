2be244478a62e253fdbca6b6fe772abe4a7b6c57
Sekhri <vsekhri@FTDCORP.NET>
2017-12-26 15:22:58 -0600

    Removed test

--- src/test/java/com/services/micro/rules/search/SearchRuleServiceApplicationTests.java
+++ src/test/java/com/services/micro/rules/search/SearchRuleServiceApplicationTests.java
@@ -1,14 +1,9 @@
 package com.services.micro.rules.search;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class SearchRuleServiceApplicationTests {
-    @Test
-    public void contextLoads() {
-    }
+//    @Test
+//    public void contextLoads() {
+//    }
 }

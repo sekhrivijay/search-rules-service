22b2012d086206556ba393d65d23674ead50c697
Sekhri <vsekhri@FTDCORP.NET>
2017-12-26 15:10:53 -0600

    PMD fixes

--- src/main/java/com/services/micro/rules/search/SearchRuleServiceApplication.java
+++ src/main/java/com/services/micro/rules/search/SearchRuleServiceApplication.java
@@ -1,12 +1,9 @@
 package com.services.micro.rules.search;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
